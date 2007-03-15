f0d3eb304cd73980ebdf07aa9cdec9c38ff69058
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2007-03-15 17:14:18 +0000

    changes for new unit tests

--- test/stubs/SealModule.cc
+++ test/stubs/SealModule.cc
@@ -2,13 +2,18 @@
 #include "PluginManager/ModuleDef.h"
 DEFINE_SEAL_MODULE();
 
-#include "DataFormats/SiStripCommon/test/stubs/test_SiStripHistoNamingScheme.h"
-DEFINE_ANOTHER_FWK_MODULE(test_SiStripHistoNamingScheme);
-
 #include "DataFormats/SiStripCommon/test/stubs/test_SiStripFecKey.h"
 DEFINE_ANOTHER_FWK_MODULE(test_SiStripFecKey);
 
 #include "DataFormats/SiStripCommon/test/stubs/test_SiStripFedKey.h"
 DEFINE_ANOTHER_FWK_MODULE(test_SiStripFedKey);
 
+#include "DataFormats/SiStripCommon/test/stubs/test_SiStripNullKey.h"
+DEFINE_ANOTHER_FWK_MODULE(test_SiStripNullKey);
+
+#include "DataFormats/SiStripCommon/test/stubs/test_SiStripEnumsAndStrings.h"
+DEFINE_ANOTHER_FWK_MODULE(test_SiStripEnumsAndStrings);
+
+#include "DataFormats/SiStripCommon/test/stubs/test_SiStripHistoTitle.h"
+DEFINE_ANOTHER_FWK_MODULE(test_SiStripHistoTitle);
 
